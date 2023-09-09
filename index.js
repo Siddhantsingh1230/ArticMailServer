997680d2003d230b1c19fe485f9ca8809725d822
Siddhantsingh1230 <Siddhantsingh9023134084@gmail.com>
2023-09-09 15:13:22 +0530

    added project

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ const forgotpwd = async (req, res) => {
     email,
     _id: user._id,
   };
-  const token = jwt.sign(payload, secret, { expiresIn: "15m" });
+  const token = jwt.sign(payload, secret, { expiresIn: "10" });
   const link = `https://articverse.vercel.app/resetpwd/${user._id}/${token}`;
   sendResetMail(
     user.firstname,
